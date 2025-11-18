33d88b7638c5a2822a66a7fd594db8e1d789592c
MEminerd <memin.erdo@gmail.com>
2025-11-18 20:27:21 +0300

    Update Intent.java

--- src/main/java/rag/intents/Intent.java
+++ src/main/java/rag/intents/Intent.java
@@ -1,5 +1,9 @@
 package rag.intents;
 
 public enum Intent {
-    STAFF_LOOKUP, COURSE_INFO, POLICY, UNKNOWN
+    Registration,       // User queries about course registration, enrollment, etc.
+    StaffLookup,        // User queries about finding staff, professors, faculty members
+    PolicyFAQ,          // User queries about university policies, rules, FAQs
+    Course,             // User queries about courses, classes, schedules, syllabi
+    Unknown             // When Intent cannot be determined
 }
