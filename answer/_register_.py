a5c25e8abe8f39ee9f86c0fb75155575790c1815
MEminerd <memin.erdo@gmail.com>
2025-11-23 13:11:53 +0300

    Create _register_.py

---
+++ answer/_register_.py
@@ -0,0 +1,13 @@
+from typing import Dict
+
+from rag.answer.answer_agent import AnswerAgent
+from rag.answer.template_answer_agent import TemplateAnswerAgent
+
+
+class _register_:
+  
+    _agents: Dict[str, AnswerAgent] = {
+    }
+    @staticmethod
+    def get(name: str) -> AnswerAgent:
+        return _register_._agents.get(name)
